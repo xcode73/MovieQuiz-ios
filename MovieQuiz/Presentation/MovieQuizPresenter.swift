44f0643a070049ff1208073d1588ce461a39236a
xcode73 <nikolai.dev@icloud.com>
2024-05-23 08:56:10 +0400

    refactor: move QuestionFactoryDelegate methods to presenter

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -10,7 +10,26 @@ import UIKit
 final class MovieQuizPresenter {
     
     let questionsAmount: Int = 10
+    
+    var currentQuestion: QuizQuestion?
+    var correctAnswers: Int = 0
+    
+    private var questionFactory: QuestionFactoryProtocol?
+    private weak var viewController: MovieQuizViewController?
     private var currentQuestionIndex: Int = 0
+    
+    init(viewController: MovieQuizViewController) {
+        self.viewController = viewController
+        
+        loadQuiz()
+    }
+    
+    func loadQuiz() {
+        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(networkClient: NetworkClient()), delegate: self)
+        questionFactory?.loadData()
+        viewController?.showSpinner()
+    }
+    
     
     /// Конвертация модели вопроса во вью модель вопроса
     /// - Parameter model: модель вопроса
@@ -35,4 +54,80 @@ final class MovieQuizPresenter {
         currentQuestionIndex += 1
     }
     
+    /// Cохранение верного ответа
+    /// - Parameter answer: Ответ пользователя
+    func storeAnswer(with answer: Bool) {
+        if answer == currentQuestion?.correctAnswer {
+            correctAnswers += 1
+        }
+    }
+    
+    /// Перезапуск игры
+    ///
+    /// Сброс текущей статистики и запрос нового вопроса
+    func restartQuiz() {
+        resetQuestionIndex()
+        correctAnswers = 0
+        viewController?.showSpinner()
+        questionFactory?.requestNextQuestion()
+    }
+    
+    /// Вывод следующего вопроса или результата квиза
+    /// - Parameter answer: Нажата кнопка: Да - true; Нет - false.
+    func buttonAction(with givenAnswer: Bool) {
+        storeAnswer(with: givenAnswer)
+        viewController?.showAnswer(with: givenAnswer)
+        
+        if isLastQuestion() {
+            viewController?.showResults()
+        } else {
+            updateQuestionIndex()
+            showNextQuestion()
+        }
+    }
+    
+    /// Вывод следующего вопроса с задержкой в 1 секунду
+    func showNextQuestion() {
+        viewController?.showSpinner()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            self?.questionFactory?.requestNextQuestion()
+        }
+    }
+}
+
+// MARK: - QuestionFactoryDelegate
+
+extension MovieQuizPresenter: QuestionFactoryDelegate {
+    
+    /// Cообщение об успешной загрузке
+    func didLoadDataFromServer() {
+        questionFactory?.requestNextQuestion()
+    }
+    
+    /// Cообщение об ошибке загрузки игры
+    /// - Parameter error: Описание ошибки
+    func didFailToLoadData(with error: Error) {
+        let message = error.localizedDescription
+        viewController?.hideSpinner()
+        viewController?.showNetworkError(message: message)
+    }
+    
+    /// Сообщение об получении следующего вопроса
+    /// - Parameter question: Вопрос
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        guard let question = question else { return }
+
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.viewController?.showQuestion(quiz: viewModel)
+        }
+    }
+    
+    /// Cообщение об ошибке при получении вопроса
+    func didFailToReceiveNextQuestion() {
+        viewController?.hideSpinner()
+        viewController?.showQuestionsAlert()
+    }
 }
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,11 +9,6 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     
-    // MARK: - Public properties
-    var questionFactory: QuestionFactory?
-//    lazy var currentQuestionIndex: Int = 0
-    lazy var correctAnswers: Int = 0
-    
     // MARK: - IBOutlets
     @IBOutlet
     private var imageView: UIImageView!
@@ -35,90 +30,37 @@ final class MovieQuizViewController: UIViewController {
     
     // MARK: - Private properties
     private lazy var statisticService: StatisticServiceProtocol = StatisticService()
-    private var currentQuestion: QuizQuestion?
-//    private let questionsAmount: Int = 10
-    private let presenter = MovieQuizPresenter()
+    
+    private var presenter: MovieQuizPresenter!
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if traitCollection.userInterfaceStyle == .light {
-            showThemeAlert()
-            overrideUserInterfaceStyle = .dark
-        }
-        
-        loadQuiz()
+        presenter = MovieQuizPresenter(viewController: self)
+        setDefaultTheme(style: .dark)
     }
     
     //MARK: - Private methods
     
-    /// Загрузка вопрос
-    private func loadQuiz() {
-        showSpinner()
-        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(networkClient: NetworkClient()), delegate: self)
-        questionFactory?.loadData()
-    }
-    
-    /// Конвертация модели вопроса во вью модель вопроса
-    /// - Parameter model: модель вопроса
-    /// - Returns: вью модель для экрана вопроса
-//    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-//        let questionStep = QuizStepViewModel(
-//            image: UIImage(data: model.image) ?? UIImage(),
-//            question: model.text,
-//            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
-//        return questionStep
-//    }
-    
     /// Вывод вопроса
     /// - Parameter step: вью модель для экрана вопроса
-    private func showQuestion(quiz step: QuizStepViewModel) {
+    func showQuestion(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
         
-        imageView.layer.borderColor = UIColor.clear.cgColor
+        resetAnswer()
         hideSpinner()
-    }
-    
-    /// Вывод следующего вопроса или результата квиза
-    /// - Parameter answer: Нажата кнопка: Да - true; Нет - false.
-    private func buttonAction(with givenAnswer: Bool) {
-        storeAnswer(with: givenAnswer)
-        showAnswer(with: givenAnswer)
-        
-        if presenter.isLastQuestion() {
-            showResults()
-        } else {
-            presenter.updateQuestionIndex()
-            showNextQuestion()
-        }
-    }
-    
-    /// Вывод следующего вопроса с задержкой в 1 секунду
-    private func showNextQuestion() {
-        showSpinner()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            self?.questionFactory?.requestNextQuestion()
-        }
-    }
-    
-    /// Cохранение верного ответа
-    /// - Parameter answer: Ответ пользователя
-    private func storeAnswer(with answer: Bool) {
-        if answer == currentQuestion?.correctAnswer {
-            correctAnswers += 1
-        }
     }
     
     /// Изменение цвета рамки постера в зависимости от ответа.
     /// - Parameter answer: Ответ пользователя
-    private func showAnswer(with answer: Bool) {
+    func showAnswer(with answer: Bool) {
         let color: CGColor?
         
-        if answer == currentQuestion?.correctAnswer {
+        if answer == presenter.currentQuestion?.correctAnswer {
             color = UIColor.ypGreen.cgColor
         } else {
             color = UIColor.ypRed.cgColor
@@ -127,41 +69,42 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderColor = color
     }
     
-    /// Перезапуск игры
-    ///
-    /// Сброс текущей статистики и запрос нового вопроса
-    private func restartQuiz() {
-        presenter.resetQuestionIndex()
-        correctAnswers = 0
-        showSpinner()
-        questionFactory?.requestNextQuestion()
-    }
-    
     /// Вывод индикатора загрузки
-    private func showSpinner() {
+    func showSpinner() {
         activityIndicator.startAnimating()
         loadingView.isHidden = false
         view.isUserInteractionEnabled = false
     }
-
-    private func hideSpinner() {
+    
+    /// Скрытие индикатора загрузки
+    func hideSpinner() {
         activityIndicator.stopAnimating()
         loadingView.isHidden = true
         view.isUserInteractionEnabled = true
     }
     
+    private func setDefaultTheme(style: UIUserInterfaceStyle) {
+        if traitCollection.userInterfaceStyle == .light {
+            showThemeAlert()
+            overrideUserInterfaceStyle = style
+        }
+    }
     
+    /// Изменение цвета рамки постера на прозрачный
+    private func resetAnswer() {
+        imageView.layer.borderColor = UIColor.clear.cgColor
+    }
     
     //MARK: - IBActions
     
     @IBAction
     private func yesButtonClicked(_ sender: UIButton) {
-        buttonAction(with: true)
+        presenter.buttonAction(with: true)
     }
     
     @IBAction
     private func noButtonClicked(_ sender: UIButton) {
-        buttonAction(with: false)
+        presenter.buttonAction(with: false)
     }
     
     /// Тема оформления по выбору системы
@@ -170,44 +113,8 @@ final class MovieQuizViewController: UIViewController {
     }
 }
 
-// MARK: - QuestionFactoryDelegate
-
-extension MovieQuizViewController: QuestionFactoryDelegate {
-    
-    /// Cообщение об успешной загрузке
-    func didLoadDataFromServer() {
-        questionFactory?.requestNextQuestion()
-    }
-    
-    /// Cообщение об ошибке загрузки игры
-    /// - Parameter error: Описание ошибки
-    func didFailToLoadData(with error: any Error) {
-        hideSpinner()
-        showNetworkError(message: error.localizedDescription)
-    }
-    
-    /// Сообщение об получении следующего вопроса
-    /// - Parameter question: Вопрос
-    func didReceiveNextQuestion(question: QuizQuestion?) {
-        guard let question = question else { return }
-
-        currentQuestion = question
-        let viewModel = presenter.convert(model: question)
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.showQuestion(quiz: viewModel)
-        }
-    }
-    
-    func didFailToReceiveNextQuestion() {
-        hideSpinner()
-        showQuestionsAlert()
-    }
-}
-
-
 // MARK: - Alerts
-private extension MovieQuizViewController {
+extension MovieQuizViewController {
     
     /// Вывод результата игры
     ///
@@ -218,7 +125,7 @@ private extension MovieQuizViewController {
             AlertPresenter.resultAlert(on: self, 
                                        with: convertResultToAlert(model: createResults()),
                                        completion: { [weak self] in
-                self?.restartQuiz()
+                self?.presenter.restartQuiz()
             })
         }
     }
@@ -227,14 +134,14 @@ private extension MovieQuizViewController {
     /// - Parameter message: Описание ошибки
     func showNetworkError(message: String) {
         AlertPresenter.networkErrorAlert(on: self, with: message, completion: { [weak self] in
-            self?.loadQuiz()
+            self?.presenter.loadQuiz()
         })
     }
     
     /// Вывод сообщения о проблемах с сетью
     func showQuestionsAlert() {
         AlertPresenter.showQuestionNetworkError(on: self, completion: { [weak self] in
-            self?.showNextQuestion()
+            self?.presenter.showNextQuestion()
         })
     }
     
@@ -247,11 +154,11 @@ private extension MovieQuizViewController {
     
     /// Создание результата игры
     func createResults() -> QuizResultsViewModel {
-        statisticService.store(correct: correctAnswers, total: presenter.questionsAmount)
+        statisticService.store(correct: presenter.correctAnswers, total: presenter.questionsAmount)
         
         let text =
                     """
-                    Ваш результат: \(correctAnswers)/\(presenter.questionsAmount)
+                    Ваш результат: \(presenter.correctAnswers)/\(presenter.questionsAmount)
                     Количество сыгранных квизов: \(statisticService.gamesCount)
                     Рекорд: \(statisticService.bestGame.correct)/\(presenter.questionsAmount) (\(statisticService.bestGame.date.dateTimeString))
                     Средняя точность: \(String(format: "%.2f", statisticService.totalAccuracy))%
